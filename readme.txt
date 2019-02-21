77f7e74d53e5e99164c119a3e7f69686bd44eed2
vaguelyr <greymoth90@gmail.com>
2019-02-21 12:25:05 -0600

    new file:   readme.txt

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Every n seconds gets a frame from an earthcam.com stream and sets it as your wallpaper.
+
+The earthcam.com stream's webplayer continually downloads a m3u8 playlist file that holds links to video chunks that it plays.
+
+This downloads the newest video chunk from that playlist file and sets it as your wallpaper, every n seconds. 
--- stream_wallpaper.sh
+++ stream_wallpaper.sh
@@ -7,6 +7,8 @@
 # the webplayer plays those chunks and continually gets the playlist to get more chunks
 
 # TODO cleanup the video playlist url part
+# TODO move the wallpaper setting part into a function so we can make a new block for each desktop environment and comment out ones for other desktop environments and keep the script cleaner
+
 
 # nashville woods
 videourl=https://videos3.earthcam.com/fecnetwork/13650.flv/chunklist_w334136231.m3u8
@@ -23,7 +25,7 @@ while true ; do
 	# create and cleanup working space	
 	popd	
 	rm -rf temp
-	mkdir temp
+	mkdir temp # if this becomes a problem we can do it in /tmp
 	pushd temp
 
 	# get the next video chunk	
@@ -33,6 +35,7 @@ while true ; do
 	ffmpeg -i m*.ts -ss 00:00:00 -vframes 1 -q:v 1 image.jpg 
 
 	# set wallpaper
+	#	this works on mate 
 	gsettings set org.mate.background picture-filename $(pwd)/image.jpg
 
 	sleep $((time+1))
