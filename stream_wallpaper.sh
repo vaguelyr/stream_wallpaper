e55000e8354d17369041f8adcd195319f5b8e719
vaguelyr <greymoth90@gmail.com>
2019-02-22 11:57:53 -0600

    Cleaned up video url section

--- stream_wallpaper.sh
+++ stream_wallpaper.sh
@@ -1,26 +1,40 @@
 #/bin/bash
 # vague
-# get wallpapers from livestream
+# get and set wallpapers from livestream
 
-# the webplayer knows where the playlist is
-# the playlist holds the next 3 video chunks
-# the webplayer plays those chunks and continually gets the playlist to get more chunks
-
-# TODO cleanup the video playlist url part
 # TODO move the wallpaper setting part into a function so we can make a new block for each desktop environment and comment out ones for other desktop environments and keep the script cleaner
 
+# handle losing internet cleanly
+wget(){
+	command wget $@ || {
+		echo failed. waiting 10s >&2
+		sleep 10s
+		wget $@
+	}
+}
 
-# nashville woods
-videourl=https://videos3.earthcam.com/fecnetwork/13650.flv/chunklist_w334136231.m3u8
-# nashville city
-videourl=https://videos3.earthcam.com/fecnetwork/13714.flv/chunklist_w597565875.m3u8
+# address of the camera from the proper user facing website 
+#	ive only tested this with earthcam.com links
+vidurl='https://www.earthcam.com/world/russia/moscow/?cam=moscow_hd'
+vidurl='https://www.earthcam.com/usa/illinois/chicago/field/?cam=fieldmuseum'
+vidurl='https://www.earthcam.com/usa/tennessee/nashville/?cam=nashville'
 
 # time between wallpaper updates in seconds
 time=10
 
+# find the chunklist stream
+linkToChunkListLink=$(wget -q -O - $vidurl  | grep ,\"html5_streamingdomain.*m3u8 -o | cut -d ':' -f 2,3,4 | sed -e 's+\\++g' -e 's+","html5_streampath":"++' | grep http.*m3u8 -o)
+echo linkToChunkListLink $linkToChunkListLink
+urlbase=$(echo $linkToChunkListLink | sed -e 's/playlist.*//')
+echo using urlbase $urlbase
+chunkListLink=$urlbase$(wget -q -O - $linkToChunkListLink | grep chunklist)
+echo chunkListLink $chunkListLink
+#latestChunk=$urlbase$(wget -q -O - $chunkListLink | tac | grep -m1 'media.*ts')
+#echo latestChunk $latestChunk
+
 while true ; do 
-	latesttsurl=https://videos3.earthcam.com/fecnetwork/13714.flv/$(wget -q -O - $videourl | tail -n1)
-	echo $latesttsurl
+	latestChunk=$urlbase$(wget -q -O - $chunkListLink | tac | grep -m1 'media.*ts')
+	echo $latestChunk
 
 	# create and cleanup working space	
 	popd	
@@ -29,7 +43,7 @@ while true ; do
 	pushd temp
 
 	# get the next video chunk	
-	wget $latesttsurl 
+	wget $latestChunk
 
 	# extract image from video for wallpaper
 	ffmpeg -i m*.ts -ss 00:00:00 -vframes 1 -q:v 1 image.jpg 
@@ -38,5 +52,5 @@ while true ; do
 	#	this works on mate 
 	gsettings set org.mate.background picture-filename $(pwd)/image.jpg
 
-	sleep $((time+1))
+	sleep $time
 done
